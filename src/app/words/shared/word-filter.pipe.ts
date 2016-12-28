4f4892dc270f34b7cf9e225c643bbd2fc5463fbb
noprysk <noprysk@n-ix.com>
2016-12-28 13:52:32 +0200

    word search

--- src/app/words/shared/word-filter.pipe.ts
+++ src/app/words/shared/word-filter.pipe.ts
@@ -1,17 +1,14 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import {WordModel} from "./word.model";
-import {FirebaseListObservable} from "angularfire2";
 
 @Pipe({name: 'wordFilter'})
 export class WordFilterPipe implements PipeTransform {
 
-  transform(words: FirebaseListObservable<WordModel[]>, filter: string) {
-      words.subscribe(wordlist =>
-        wordlist.filter(word =>
+  transform(words: WordModel[], filter: string) {
+      words.filter(word =>
           word.word == '' ||
           word.word.indexOf(filter) > -1 ||
-          word.translation.indexOf(filter) > -1)
-      );
+          word.translation.indexOf(filter) > -1);
   }
 
 }
