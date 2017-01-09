47842072651c1b137062508d5ba7a8c361165520
CycleDev <CycleDev@gmail.com>
2017-01-09 23:23:05 +0200

    fix print functionality

--- src/app/common/app.settings.ts
+++ src/app/common/app.settings.ts
@@ -1,5 +1,5 @@
 
 export class AppSettings {
-  public static get REST_API_URL(): string { return 'http://en-words-api.herokuapp.com/enwords/api'; }
+  public static get REST_API_URL(): string { return 'https://en-words-api.herokuapp.com/enwords/api'; }
   public static get Version(): string { return '0.0.2'; }
 }
