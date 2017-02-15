320c5ac1ac57ddbe9f9d177e2bc100acae369163
noprysk <noprysk@n-ix.com>
2017-02-15 17:34:21 +0200

    Trainings component.

--- src/components/trainings/Trainings.js
+++ src/components/trainings/Trainings.js
@@ -1,39 +1,79 @@
 import React from 'react';
-import { browserHistory } from 'react-router';
-import { Button, Modal, Spin, Menu } from 'antd';
-import GroupForm from '../../containers/GroupFormContainer';
 
-const ButtonGroup = Button.Group;
+import { Table, Button } from 'antd';
 
 class Trainings extends React.Component {
 
-    constructor(props) {
-        super(props);
+    //constructor(props) {
+    //    super(props);
 
         //this.handelEditClick = this.handelEditClick.bind(this);
-    }
+    //}
 
     componentDidMount() {
         this.props.fetchTrainings();
     }
 
     render() {
-        const { trainings, error, loading } = this.props.trainingList;
+        const { trainings, error } = this.props.trainingList;
 
-        if(loading) {
-            return <Spin />
-        } else if(error) {
+        if(error) {
             return <div className="alert alert-danger">Error: {error.message}</div>
         }
 
+        let content = <Table
+            rowKey={record => record.trainingId}
+            dataSource={trainings}
+            columns={[{
+                    title: '',
+                    dataIndex: '',
+                    key: '',
+                    width: '80px',
+                    render: (text, record) => {
+                        if (record.result >= 65) {
+                            return 'Passed';
+                        } else {
+                            return 'Failed';
+                        }
+                    }
+                },
+                {
+                    title: 'Group',
+                    dataIndex: 'groupName',
+                    key: 'groupName',
+                    //sorter: (a, b) => this.compareWord(a, b),
+                    //render:  (text, record) => {record.groupName}
+                },
+                {
+                    title: 'Result',
+                    dataIndex: 'result',
+                    key: 'result',
+                    //sorter: (a, b) => this.compareTranslation(a, b)
+                    render:  (text, record) => `${record.result}%`
+                },
+                {
+                    title: 'Date',
+                    dataIndex: 'trainingDate',
+                    key: 'trainingDate',
+                    render:  (text, record) => {
+                        return new Date(record.trainingDate).toLocaleString();
+                    }
+                },
+                {
+                    title: '',
+                    dataIndex: 'delete',
+                    key: 'delete',
+                    width: '20px',
+                    render: (text, record) => (
+                        <Button id="deleteTraining" shape="circle" icon="delete" />
+                    )
+                }]}
+            size="middle"
+            pagination={false}/>;
+
         return (
             <div>
-                { trainings.map(training =>
-                    <div key={training.trainingId}>
-                        <p>{training.result}</p>
-                        <p>{training.groupId}</p>
-                    </div>
-                )}
+                {content}
             </div>
         )
     }
