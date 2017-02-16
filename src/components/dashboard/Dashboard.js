365b25ddd1ecc8be8a170039541f903736ded13d
noprysk <noprysk@n-ix.com>
2017-02-16 14:46:11 +0200

    Hide trainings component.

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import Trainings from '../../containers/TrainingsContainer';
 
 class Dashboard extends Component {
+
   render() {
     return (
         <div>
             <h3>Dashboard</h3>
             <p>This application can be used as online dictionary for language courses.</p>
-            <Trainings />
         </div>
     );
   }
