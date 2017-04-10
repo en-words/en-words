74f693f37bce0dbf6343e398848d31400eee07c0
noprysk <noprysk@n-ix.com>
2017-04-10 22:15:46 +0300

    Update firebase project

--- src/database.js
+++ src/database.js
@@ -1,10 +1,12 @@
 import firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyBxPj7zxNJwFohR3RdJ3WiaTGo1v1kg-Kk",
-    authDomain: "en-words-react.firebaseapp.com",
-    databaseURL: "https://en-words-react.firebaseio.com",
-    storageBucket: "en-words-react.appspot.com"
+    apiKey: "AIzaSyB62Fl41DtDxTtezlrkdvUWwgfQBrRUEKI",
+    authDomain: "learn-en-words.firebaseapp.com",
+    databaseURL: "https://learn-en-words.firebaseio.com",
+    projectId: "learn-en-words",
+    storageBucket: "learn-en-words.appspot.com",
+    messagingSenderId: "332194151648"
 };
 
 firebase.initializeApp(config);
